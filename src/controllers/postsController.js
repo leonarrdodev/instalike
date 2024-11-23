0c1db665f8cfd36b24930fb7f00144ccf5e65d24
Leonardo <leonardo.pro.7004@gmail.com>
2024-11-23 13:07:10 -0300

    deploy projeto

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,6 @@
 import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
 import fs from 'fs';
+import gerarDescricaoComGemini from "../services/geminiService.js";
 
 export async function listarPosts(req, res) {
     // Obtendo os posts do banco de dados
@@ -39,12 +40,15 @@ export async function uploadImagem(req, res){
 export async function atualizarNovoPost(req, res){
     const id = req.params.id;
     const urlImagem = `http://localhost:3000/${id}.png`;
-    const post = {
-        imgUrl: urlImagem,
-        descricao: req.body.descricao,
-        alt: req.body.alt
-    }
+
     try{
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer)
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
         const postCriado = await atualizarPost(id, post);
         res.status(200).json(postCriado);
     } catch(erro){
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import multer from 'multer';
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from '../controllers/postsController.js';
+import cors from 'cors';
+
+const corsOptions = {
+    origin: 'http://localhost:8000',
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,6 +22,7 @@ const upload = multer({ dest: "./uploads" , storage});
 const routes = (app) => {
     // Habilitando o parser JSON para lidar com requisições com corpo JSON
     app.use(express.json());
+    app.use(cors(corsOptions))
     // Rota para obter todos os posts
     app.get('/posts', listarPosts);
     //Rota para criar um novo post
